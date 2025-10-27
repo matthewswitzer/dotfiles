cbc4920bcc77412b2d8f75d6ce67f3ff05e01309
Matthew Switzer <54642197+matthewswitzer@users.noreply.github.com>
2025-10-26 22:00:32 -0400

    Changed complete trigger to <M-Space>

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -53,7 +53,7 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(),
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-u>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<M-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm {
                         behavior = cmp.ConfirmBehavior.Replace,
