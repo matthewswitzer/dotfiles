38619330b44a9eeb458a069e3c9b973ee8ac46b1
Matthew Switzer <54642197+matthewswitzer@users.noreply.github.com>
2025-12-26 12:06:08 -0500

    Embedded image support (image.nvim)

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -143,6 +143,20 @@ return {
         end,
     },
 
+    -- image support
+    {
+        '3rd/image.nvim',
+        opts = {
+            backend = 'ueberzug',
+            max_width = 100,
+            max_height = 12,
+            max_height_window_percentage = math.huge,
+            max_width_window_percentage = math.huge,
+            window_overlap_clear_enabled = true,
+            window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
+        },
+    },
+
     -- file explorer
     {
         'nvim-neo-tree/neo-tree.nvim',
